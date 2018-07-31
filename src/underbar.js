9c2d917a91fae2a96e108e2c87b03a72f9f00f85
shabbyjoon <samokhtarani@yahoo.com>
2018-07-31 11:24:40 -0700

    Finished upto uniq and working on uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.slice(Math.max(array.length-n, 0)); 
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }    
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +77,40 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+   var output = [];
+   _.each(collection, function(elem){
+    if(test(elem)){
+    output.push(elem);
+    }
+   })
+    return output;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(elem){
+      return !test(elem);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var output = [];
+    var result = [];
+    _.each(array, function(elem){
+      if (iterator){
+        output.push(iterator(elem)); //[true, false, false, false, false, false]
+        if(_.indexOf(output, iterator(elem)) === -1){
+        output.push(elem);
+      }  
+        } else { if(_.indexOf(output, elem) === -1){
+        output.push(elem); 
+        } 
+      }          
+    })
+    return output;
   };
 
 
